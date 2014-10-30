af9015baec35c2aa0de1ab39408587d8712092e8
DominicMorquecho <dominic.morquecho@gmail.com>
2014-10-30 10:52:25 -0700

    Created a table for blog posts

--- controller/create-db.php
+++ controller/create-db.php
@@ -19,6 +19,12 @@
  			echo "Database has already been created";
  		}
 /*test comment for source tree*/
+	$query = $connection->query("CREATE TABLE posts (" /*creates a table called posts that needs....*/
+		. "id int(11) NOT NULL AUTO_INCREMENT," /*needs a id that will be an integer, cant be empty*/
+		. "title varchar (255) NOT NULL," /*needsa title up to 255 characters and cant be nothing*/
+		. "post text NOT NULL,"  /*the actual text of the post, Cant be empty*/
+		. "PRIMARY KEY (id)"); /*the tables primary key is the id*/
+
  	$connection->close();
 
 ?>
