4041d7d0439fb69e58fada060eb6d2fb90ba27e4
DominicMorquecho <dominic.morquecho@gmail.com>
2014-11-03 12:00:15 -0800

    Created a navigation and post file. the navigation file added a link that goes to post.php (that has nothng in it)

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <?php 
 	require_once(__DIR__ . "/view/header.php"); /*puts a header*/
+	require_once(__DIR__ . "/view/navigation.php"); /*inserts the code in navigation.php*/
 	require_once(__DIR__ . "/controller/create-db.php"); // puts everything in create-db here
 	require_once(__DIR__ . "/view/footer.php"); /*puts a footer*/
 ?>
---
+++ view/navigation.php
@@ -0,0 +1,5 @@
+<nav> 
+<ul>
+	<li><a href="../post.php">Blog Post Form</a></li> <!-- link that goes to post.php -->
+</ul>
+</nav>
