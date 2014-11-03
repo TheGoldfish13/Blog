445637b06adfdd177572be3179a5fa3be019876f
DominicMorquecho <dominic.morquecho@gmail.com>
2014-11-03 11:46:54 -0800

    added header and footer

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,16 +1,12 @@
 <?php
 	require_once(__DIR__ . "/../model/database.php"); //takes code from database and connects to server
-
  	$connection = new mysqli($host, $username, $password); //the variable used to connect to server
-
  	if($connection->connect_error) { //if it doesnt connect kill the program, and echo the error
  		die("Error: " . $connection->connection_error);
  	} 
  	$exists = $connection->select_db($database); // It trys to connect to the MYSQL server
-
  	if(!$exists) { // if the database doesnt exist it makes one
  		$query = $connection->query("CREATE DATABASE $database");
-
  		if($query) { 
  			echo "Succesfully created database: " . $database;
  		}
@@ -23,8 +19,14 @@
 		. "id int(11) NOT NULL AUTO_INCREMENT," /*needs a id that will be an integer, cant be empty*/
 		. "title varchar (255) NOT NULL," /*needsa title up to 255 characters and cant be nothing*/
 		. "post text NOT NULL,"  /*the actual text of the post, Cant be empty*/
-		. "PRIMARY KEY (id)"); /*the tables primary key is the id*/
+		. "PRIMARY KEY (id) )"); /*the tables primary key is the id*/
+
+if($query) {
+		echo " Succesfully created table: posts"; //runs the query, creates the table once
+	}
+	else {
+		echo "<p> $connection->error </p>"; //echos out that the table has already been created
+	}
 
  	$connection->close();
-
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title></title>
-</head>
-<body>
-	<?php 
-		require_once(__DIR__ . "/controller/create-db.php"); // puts everything in creat-db in index
-	?>
-</body>
-</html>
+
+<?php 
+	require_once(__DIR__ . "/view/header.php"); /*puts a header*/
+	require_once(__DIR__ . "/controller/create-db.php"); // puts everything in create-db here
+	require_once(__DIR__ . "/view/footer.php"); /*puts a footer*/
+?>
