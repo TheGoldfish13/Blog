d72c033c41cf4d68c53daa26e49429e6f4c06102
DominicMorquecho <dominic.morquecho@gmail.com>
2014-11-21 10:57:23 -0800

    Completed up to video 35: created the session variable to store the database object.

--- model/config.php
+++ model/config.php
@@ -1,5 +1,6 @@
 <?php
 	require_once(__DIR__ . "/database.php");
+	session_start();
 
 	$path = "/Blog/"; /*stores the path to the project folder*/
 	$host = "localhost";
@@ -7,4 +8,7 @@
 	$password = "root";
 	$database = "blog_db";
 
-	$connection = new Database($host, $username, $password, $database); /*creates a new object $connection that replaces the need for repeating the old line of code that does the same thing */
+	if(!isset($_SESSION["connection"])) { /*if the variable does not contain information than it will create the database object and set it to the session variable*/
+		$connection = new Database($host, $username, $password, $database); /*creates a new object $connection that replaces the need for repeating the old line of code that does the same thing */
+		$_SESSION["connection"] = $connection;
+	}
--- view/navigation.php
+++ view/navigation.php
@@ -5,7 +5,7 @@
 <nav> 
 <ul>
 	<p>
-		<li><a href="<?php echo $path . "post.php" ; ?>">Blog Post Form</a></li> <!-- link that goes to post.php -->
+		<li><a href="<?php echo $path . "post.php" ; ?>"><h1>Blog Post Form </h2></a></li> <!-- link that goes to post.php -->
 		<!-- concatinates the path variable with post.php to have the equivalent of href="/Blog/post.php" -->
 	</p>
 </ul>
