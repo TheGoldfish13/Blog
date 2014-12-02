98506bc688ff217384016c25e3b7381e88120f4a
DominicMorquecho <dominic.morquecho@gmail.com>
2014-12-02 11:00:05 -0800

    created the register-form.php file that contains input boxes asking for various register/login information.

---
+++ view/register-form.php
@@ -0,0 +1,19 @@
+<form>
+	<div>
+		<label for="email">Email: </label>	<!-- creates a input box for the users email to be entered -->
+		<input type="text" name="email" />
+	</div>
+	<div>
+		<label for="username">Username: </label>	<!-- input box for username -->
+		<input type="text" name="username" />
+	</div>
+	<div>
+		<label for="password">Password: </label>	<!-- input box for password -->
+		<input type="password" name="password" />
+	</div>
+
+	<div>
+		<button type="submit">Submit</button> <!-- submit button -->
+	</div>
+
+</form>
