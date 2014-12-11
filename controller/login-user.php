1a0eb96500fda3073ec28b9b775b6026d51bc777
DominicMorquecho <dominic.morquecho@gmail.com>
2014-12-11 10:56:46 -0800

    Completed video 46: Creating a login form and controller

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,2 +1,7 @@
 <?php
-	echo "successfully logged in user" ;
+	require_once(__DIR__ . "/../model/config.php"); /*puts code from config here*/
+
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING); /*filters the input to make sure that the input for username is a string*/
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING); /*filters the input to make sure that the input for password is a string*/
+
+	$query = $_SESSION["connection"]->query("SELECT salt, password FORM users = WHERE '$username'");
